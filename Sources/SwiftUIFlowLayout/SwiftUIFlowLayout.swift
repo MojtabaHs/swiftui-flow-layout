2f9f665725033e97af5465e4672fdcebb1d42c6b
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-09-03 13:30:32 +0330

    style: unify indentation with the original 2space value

--- Sources/SwiftUIFlowLayout/SwiftUIFlowLayout.swift
+++ Sources/SwiftUIFlowLayout/SwiftUIFlowLayout.swift
@@ -26,9 +26,9 @@ public struct FlowLayout<Trigger, Data, Content>: View where Data: RandomAccessC
 
   public var body: some View {
     let stack = VStack {
-       GeometryReader { geometry in
-         self.content(in: geometry)
-       }
+      GeometryReader { geometry in
+        self.content(in: geometry)
+      }
     }
     return Group {
       if mode == .scrollable {
@@ -45,33 +45,33 @@ public struct FlowLayout<Trigger, Data, Content>: View where Data: RandomAccessC
     var lastHeight = CGFloat.zero
     let itemCount = data.count
     return ZStack(alignment: .topLeading) {
-        ForEach(Array(data.enumerated()), id: \.offset) { index, item in
-            content(item)
-              .padding([.horizontal, .vertical], spacing)
-              .alignmentGuide(.leading) { dimensions in
-                if (abs(width - dimensions.width) > proxy.size.width) {
-                  width = 0
-                  height -= lastHeight
-                }
-                lastHeight = dimensions.height
-                let result = width
-                if index == itemCount - 1 {
-                  width = 0
-                } else {
-                  width -= dimensions.width
-                }
-                return result
-              }
-              .alignmentGuide(.top) { _ in
-                let result = height
-                if index == itemCount - 1 {
-                  height = 0
-                }
-                return result
-              }
-        }
+      ForEach(Array(data.enumerated()), id: \.offset) { index, item in
+        content(item)
+          .padding([.horizontal, .vertical], spacing)
+          .alignmentGuide(.leading) { dimensions in
+            if (abs(width - dimensions.width) > proxy.size.width) {
+              width = 0
+              height -= lastHeight
+            }
+            lastHeight = dimensions.height
+            let result = width
+            if index == itemCount - 1 {
+              width = 0
+            } else {
+              width -= dimensions.width
+            }
+            return result
+          }
+          .alignmentGuide(.top) { _ in
+            let result = height
+            if index == itemCount - 1 {
+              height = 0
+            }
+            return result
+          }
       }
-      .background(HeightReaderView(trigger: $totalHeight))
+    }
+    .background(HeightReaderView(trigger: $totalHeight))
   }
 
   public enum Mode {
@@ -91,7 +91,7 @@ private struct HeightReaderView: View {
   var body: some View {
     GeometryReader { geo in
       Color.clear
-           .preference(key: HeightPreferenceKey.self, value: geo.frame(in: .local).size.height)
+        .preference(key: HeightPreferenceKey.self, value: geo.frame(in: .local).size.height)
     }
     .onPreferenceChange(HeightPreferenceKey.self) { h in
       trigger = h
@@ -102,65 +102,65 @@ private struct HeightReaderView: View {
 // MARK: Trigger-less Convenience Initializer
 
 public extension FlowLayout where Trigger == Never? {
-    init(mode: Mode,
-         data: Data,
-         spacing: CGFloat = flowLayoutDefaultItemSpacing,
-         @ViewBuilder content: @escaping (Data.Element) -> Content) {
-        self.init(mode: mode,
-                  trigger: .constant(nil),
-                  data: data,
-                  spacing: spacing,
-                  content: content)
-    }
+  init(mode: Mode,
+       data: Data,
+       spacing: CGFloat = flowLayoutDefaultItemSpacing,
+       @ViewBuilder content: @escaping (Data.Element) -> Content) {
+    self.init(mode: mode,
+              trigger: .constant(nil),
+              data: data,
+              spacing: spacing,
+              content: content)
+  }
 }
 
 // MARK: - Testing Previews
 
 private var sampleData = [
-    "Some long item here", "And then some longer one",
-    "Short", "Items", "Here", "And", "A", "Few", "More",
-    "And then a very very very long long long long long long long long longlong long long long long long longlong long long long long long longlong long long long long long longlong long long long long long longlong long long long long long long long one", "and", "then", "some", "short short short ones"
+  "Some long item here", "And then some longer one",
+  "Short", "Items", "Here", "And", "A", "Few", "More",
+  "And then a very very very long long long long long long long long longlong long long long long long longlong long long long long long longlong long long long long long longlong long long long long long longlong long long long long long long long one", "and", "then", "some", "short short short ones"
 ]
 
 private func text(_ data: Any) -> some View {
-    Text(String(describing: data))
-        .font(.system(size: 12))
-        .foregroundColor(.black)
-        .padding()
-        .background(RoundedRectangle(cornerRadius: 4)
-            .border(Color.gray)
-            .foregroundColor(Color.gray))
+  Text(String(describing: data))
+    .font(.system(size: 12))
+    .foregroundColor(.black)
+    .padding()
+    .background(RoundedRectangle(cornerRadius: 4)
+      .border(Color.gray)
+      .foregroundColor(Color.gray))
 }
 
 struct FlowLayout_Previews: PreviewProvider {
   static var previews: some View {
-      FlowLayout(mode: .scrollable, data: sampleData) {
-          text($0)
-      }
-      .padding()
+    FlowLayout(mode: .scrollable, data: sampleData) {
+      text($0)
+    }
+    .padding()
   }
 }
 
 struct TestWithDeletion: View {
-    @State private var data = sampleData
+  @State private var data = sampleData
 
-    var body: some View {
-        VStack {
-            Button("Delete all") {
-                data.removeAll()
-            }
-            Button("Restore") {
-                data = sampleData
-            }
-            Button("Add one") {
-                data.append("\(Date().timeIntervalSince1970)")
-            }
-            FlowLayout(mode: .vstack, data: data) {
-                text($0)
-            }
-            .padding()
-        }
+  var body: some View {
+    VStack {
+      Button("Delete all") {
+        data.removeAll()
+      }
+      Button("Restore") {
+        data = sampleData
+      }
+      Button("Add one") {
+        data.append("\(Date().timeIntervalSince1970)")
+      }
+      FlowLayout(mode: .vstack, data: data) {
+        text($0)
+      }
+      .padding()
     }
+  }
 }
 
 struct TestWithDeletion_Previews: PreviewProvider {
@@ -170,46 +170,46 @@ struct TestWithDeletion_Previews: PreviewProvider {
 }
 
 struct TestWithRange_Previews: PreviewProvider {
-    static var previews: some View {
-        FlowLayout(mode: .scrollable,
-                   data: 1..<100) {
-            text($0)
-        }.padding()
-    }
+  static var previews: some View {
+    FlowLayout(mode: .scrollable,
+               data: 1..<100) {
+      text($0)
+    }.padding()
+  }
 }
 
 // MARK: - Migration Helpers
 
 public extension FlowLayout {
-    @available(swift, obsoleted: 1.1.0, renamed: "attemptConnection")
-    var viewMapping: (Data.Element) -> Content { content }
+  @available(swift, obsoleted: 1.1.0, renamed: "attemptConnection")
+  var viewMapping: (Data.Element) -> Content { content }
 
-    @available(swift, obsoleted: 1.1.0, renamed: "init(mode:trigger:data:spacing:content:)")
-    init(mode: Mode,
-         binding: Binding<Trigger>,
-         items: Data,
-         itemSpacing: CGFloat = flowLayoutDefaultItemSpacing,
-         @ViewBuilder viewMapping: @escaping (Data.Element) -> Content) {
-        self.init(mode: mode,
-                  trigger: binding,
-                  data: items,
-                  spacing: itemSpacing,
-                  content: viewMapping)
-    }
+  @available(swift, obsoleted: 1.1.0, renamed: "init(mode:trigger:data:spacing:content:)")
+  init(mode: Mode,
+       binding: Binding<Trigger>,
+       items: Data,
+       itemSpacing: CGFloat = flowLayoutDefaultItemSpacing,
+       @ViewBuilder viewMapping: @escaping (Data.Element) -> Content) {
+    self.init(mode: mode,
+              trigger: binding,
+              data: items,
+              spacing: itemSpacing,
+              content: viewMapping)
+  }
 }
 
 public extension FlowLayout where Trigger == Never? {
-    @available(swift, obsoleted: 1.1.0, renamed: "init(mode:data:spacing:content:)")
-    init(mode: Mode,
-         items: Data,
-         itemSpacing: CGFloat = flowLayoutDefaultItemSpacing,
-         @ViewBuilder viewMapping: @escaping (Data.Element) -> Content) {
-        self.init(
-            mode: mode,
-            trigger: .constant(nil),
-            data: items,
-            spacing: itemSpacing,
-            content: viewMapping
-        )
-    }
+  @available(swift, obsoleted: 1.1.0, renamed: "init(mode:data:spacing:content:)")
+  init(mode: Mode,
+       items: Data,
+       itemSpacing: CGFloat = flowLayoutDefaultItemSpacing,
+       @ViewBuilder viewMapping: @escaping (Data.Element) -> Content) {
+    self.init(
+      mode: mode,
+      trigger: .constant(nil),
+      data: items,
+      spacing: itemSpacing,
+      content: viewMapping
+    )
+  }
 }
